853ec7b0d4c457014e6ade28a8641e0746dd73c6
egublog <egukei0729@gmail.com>
2025-04-28 23:27:28 +0900

    add: impl Url

--- saba/saba_core/src/url.rs
+++ saba/saba_core/src/url.rs
@@ -1,4 +1,5 @@
 use alloc::string::String;
+use alloc::string::ToString;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Url {
@@ -6,4 +7,19 @@ pub struct Url {
     host: String,
     port: String,
     searchpart: String,
+}
+
+impl Url {
+    fn is_http(&mut self) -> bool {
+        if self.url.contains("http://") {
+            return true;
+        }
+        return false;
+    }
+
+    pub fn parse(&mut self) -> Result<Self,String> {
+        if !self.is_http() {
+            return Err("Only HTTP scheme is supported".to_string());
+        }
+    }
 }
