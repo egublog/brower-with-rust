0efb1db0ec87575e7941d05dd260640bd21bfc97
egublog <egukei0729@gmail.com>
2025-05-02 23:54:51 +0900

    test: url_host_port_path

--- saba/saba_core/src/url.rs
+++ saba/saba_core/src/url.rs
@@ -168,4 +168,18 @@ mod tests {
 
         assert_eq!(expected, Url::new(url).parse());
     }
+
+    #[test]
+    fn test_url_host_port_path() {
+        let url = "http://example.com:8888/index.html".to_string();
+        let expected = Ok(Url {
+            url: url.clone(),
+            host: "example.com".to_string(),
+            port: "8888".to_string(),
+            path: "/index.html".to_string(),
+            searchpart: "".to_string(),
+        });
+
+        assert_eq!(expected, Url::new(url).parse());
+    }
 }
